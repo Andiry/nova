3304585d76aa7aedb5aad81fba161568661f4e66
Andiry Xu <jix024@cs.ucsd.edu>
2016-08-02 16:10:44 -0700

    Lock inode for dax fault

--- dax.c
+++ dax.c
@@ -969,12 +969,23 @@ static int nova_dax_file_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 
 static int nova_dax_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 {
+	struct inode *inode = file_inode(vma->vm_file);
 	int ret = 0;
 	timing_t fault_time;
 
 	NOVA_START_TIMING(mmap_fault_t, fault_time);
 
+	if (vmf->flags & FAULT_FLAG_WRITE) {
+		sb_start_pagefault(inode->i_sb);
+		file_update_time(vma->vm_file);
+	}
+
+	inode_lock(inode);
 	ret = dax_fault(vma, vmf, nova_dax_get_block);
+	inode_unlock(inode);
+
+	if (vmf->flags & FAULT_FLAG_WRITE)
+		sb_end_pagefault(inode->i_sb);
 
 	NOVA_END_TIMING(mmap_fault_t, fault_time);
 	return ret;
@@ -983,12 +994,23 @@ static int nova_dax_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 static int nova_dax_pmd_fault(struct vm_area_struct *vma, unsigned long addr,
 	pmd_t *pmd, unsigned int flags)
 {
+	struct inode *inode = file_inode(vma->vm_file);
 	int ret = 0;
 	timing_t fault_time;
 
 	NOVA_START_TIMING(mmap_fault_t, fault_time);
 
+	if (flags & FAULT_FLAG_WRITE) {
+		sb_start_pagefault(inode->i_sb);
+		file_update_time(vma->vm_file);
+	}
+
+	inode_lock(inode);
 	ret = dax_pmd_fault(vma, addr, pmd, flags, nova_dax_get_block);
+	inode_unlock(inode);
+
+	if (flags & FAULT_FLAG_WRITE)
+		sb_end_pagefault(inode->i_sb);
 
 	NOVA_END_TIMING(mmap_fault_t, fault_time);
 	return ret;
@@ -1006,11 +1028,14 @@ static int nova_dax_pfn_mkwrite(struct vm_area_struct *vma,
 
 	sb_start_pagefault(inode->i_sb);
 	file_update_time(vma->vm_file);
+
+	inode_lock(inode);
 	size = (i_size_read(inode) + PAGE_SIZE - 1) >> PAGE_SHIFT;
 	if (vmf->pgoff >= size)
 		ret = VM_FAULT_SIGBUS;
 	else
 		ret = dax_pfn_mkwrite(vma, vmf);
+	inode_unlock(inode);
 
 	sb_end_pagefault(inode->i_sb);
 	NOVA_END_TIMING(mmap_fault_t, fault_time);
