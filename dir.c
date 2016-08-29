3f21a537b391a4d9d7f1867d2c63a28ade4db437
luzh <luzh@eng.ucsd.edu>
2016-08-28 17:46:29 -0700

    Make atomic on in-place dentry csum updates
    Signed-off-by: Andiry Xu <jix024@cs.ucsd.edu>

--- dir.c
+++ dir.c
@@ -357,11 +357,18 @@ int nova_remove_dentry(struct dentry *dentry, int dec_link, u64 tail,
 void nova_invalidate_dentries(struct super_block *sb,
 	struct nova_dentry *create_dentry, struct nova_dentry *delete_dentry)
 {
+	struct nova_dentry dentry;
+
 	if (create_dentry && old_entry_freeable(sb, create_dentry->trans_id)) {
-		create_dentry->invalid = 1;
-		delete_dentry->invalid = 1;
-		nova_update_entry_csum(create_dentry);
-		nova_update_entry_csum(delete_dentry);
+		dentry = *create_dentry;
+		dentry.invalid = 1;
+		nova_update_entry_csum(&dentry);
+		nova_memcpy_atomic(create_dentry, &dentry, 8);
+
+		dentry = *delete_dentry;
+		dentry.invalid = 1;
+		nova_update_entry_csum(&dentry);
+		nova_memcpy_atomic(delete_dentry, &dentry, 8);
 	}
 }
 
