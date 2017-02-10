078509bd5023e2c2e5f65e5e26d39bacfaafd324
Andiry Xu <jix024@cs.ucsd.edu>
2017-02-10 10:57:50 -0800

    Refine wprotect-setup script

--- wprotect-setup.sh
+++ wprotect-setup.sh
@@ -7,9 +7,10 @@ rmmod nd_pmem
 modprobe nd_pmem readonly=1
 
 rmmod nova
-insmod nova.ko measure_timing=0 inplace_data_updates=1 replica_inode=0 replica_log=0
+insmod nova.ko measure_timing=0 inplace_data_updates=0 replica_inode=1 replica_log=1 wprotect=1
 
 sleep 1
 
 mount -t NOVA -o init,wprotect /dev/pmem0 /mnt/ramdisk
+mount -t NOVA -o init,wprotect /dev/pmem1 /mnt/scratch
 
