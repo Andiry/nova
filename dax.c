9b6ceeb18cc106abbd534b2310a9d510a43ff9dc
Andiry Xu <jix024@cs.ucsd.edu>
2017-02-13 18:27:30 -0800

    Journal write entry if replica log is disabled
    Signed-off-by: Andiry Xu <jix024@cs.ucsd.edu>

--- dax.c
+++ dax.c
@@ -636,17 +636,44 @@ out:
 	return ent_blks;
 }
 
-static void nova_inplace_update_write_entry(struct super_block *sb,
+static void nova_update_write_entry(struct super_block *sb,
 	struct nova_file_write_entry *entry, u64 trans_id, u32 time,
 	u64 entry_size)
 {
-	nova_memunlock_range(sb, entry, sizeof(*entry));
 	entry->trans_id = cpu_to_le64(trans_id);
 	entry->mtime = cpu_to_le32(time);
 	entry->size = cpu_to_le64(entry_size);
 	nova_update_entry_csum(entry);
 	nova_update_alter_entry(sb, entry);
-	nova_memlock_range(sb, entry, sizeof(*entry));
+}
+
+static void nova_inplace_update_write_entry(struct super_block *sb,
+	struct nova_file_write_entry *entry, u64 trans_id, u32 time,
+	u64 entry_size)
+{
+	struct nova_sb_info *sbi = NOVA_SB(sb);
+	int cpu;
+	u64 journal_tail;
+
+	if (replica_log) {
+		nova_memunlock_range(sb, entry, sizeof(*entry));
+		nova_update_write_entry(sb, entry, trans_id, time, entry_size);
+		nova_memlock_range(sb, entry, sizeof(*entry));
+		return;
+	}
+
+	cpu = smp_processor_id();
+	spin_lock(&sbi->journal_locks[cpu]);
+	nova_memunlock_journal(sb);
+	journal_tail = nova_create_logentry_transaction(sb, entry,
+						FILE_WRITE, cpu);
+	nova_update_write_entry(sb, entry, trans_id, time, entry_size);
+
+	PERSISTENT_BARRIER();
+
+	nova_commit_lite_transaction(sb, journal_tail, cpu);
+	nova_memlock_journal(sb);
+	spin_unlock(&sbi->journal_locks[cpu]);
 }
 
 ssize_t nova_inplace_file_write(struct file *filp,
