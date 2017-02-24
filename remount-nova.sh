7e86dc747d192ae4fa1f552169f11fc4416b3bc9
Andiry Xu <jix024@cs.ucsd.edu>
2017-02-24 13:24:08 -0800

    Remount script fix

--- remount-nova.sh
+++ remount-nova.sh
@@ -4,8 +4,8 @@ echo "Unmount existing partition..."
 umount /mnt/ramdisk
 umount /mnt/scratch
 rmmod nova
-insmod nova.ko measure_timing=0 inplace_data_updates=1 replica_metadata=1 metadata_csum=1 unsafe_metadata=0 \
-			wprotect=0 mmap_cow=1 data_csum=1 data_parity=0
+insmod nova.ko measure_timing=0 inplace_data_updates=0 replica_metadata=1 metadata_csum=1 unsafe_metadata=0 \
+			wprotect=0 mmap_cow=1 data_csum=1 data_parity=1
 
 echo "Unmount done."
 sleep 1
