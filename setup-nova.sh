8e54f6c1ba12019ccc859b81c4bcb319a3ccd486
Andiry Xu <jix024@cs.ucsd.edu>
2017-02-23 23:01:52 -0800

    Add module params to setup scripts

--- setup-nova.sh
+++ setup-nova.sh
@@ -5,7 +5,7 @@ umount /mnt/ramdisk
 umount /mnt/scratch
 rmmod nova
 insmod nova.ko measure_timing=0 inplace_data_updates=0 replica_metadata=1 metadata_csum=1 unsafe_metadata=0 \
-			      data_csum=1 data_parity=0
+			wprotect=0 mmap_cow=1 data_csum=1 data_parity=0
 
 sleep 1
 
