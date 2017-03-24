023054e2cb4cc52d6eb00765498b45b90f6395bf
Andiry Xu <jix024@cs.ucsd.edu>
2017-03-24 14:42:30 -0700

    Refine setup script

--- setup-nova.sh
+++ setup-nova.sh
@@ -4,8 +4,11 @@ modprobe libcrc32c
 umount /mnt/ramdisk
 umount /mnt/scratch
 rmmod nova
-insmod nova.ko measure_timing=0 inplace_data_updates=1 replica_metadata=1 metadata_csum=1 unsafe_metadata=1 \
-			wprotect=0 mmap_cow=1 data_csum=1 data_parity=1 dram_struct_csum=1
+insmod nova.ko measure_timing=0 \
+	inplace_data_updates=1 \
+	wprotect=0 mmap_cow=1 \
+	replica_metadata=1 metadata_csum=1 unsafe_metadata=1 dram_struct_csum=1 \
+	data_csum=1 data_parity=1
 
 sleep 1
 
