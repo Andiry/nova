1ffddc578b1435e4b5cb8b055df123bc9728e365
Andiry Xu <jix024@cs.ucsd.edu>
2017-04-15 12:08:29 -0700

    Use CoW by default

--- setup-nova.sh
+++ setup-nova.sh
@@ -5,7 +5,7 @@ umount /mnt/ramdisk
 umount /mnt/scratch
 rmmod nova
 insmod nova.ko measure_timing=0 \
-	inplace_data_updates=1 \
+	inplace_data_updates=0 \
 	wprotect=0 mmap_cow=1 \
 	unsafe_metadata=1 \
 	replica_metadata=1 metadata_csum=1 dram_struct_csum=1 \
--- wprotect-setup.sh
+++ wprotect-setup.sh
@@ -7,7 +7,7 @@ rmmod nd_pmem
 modprobe nd_pmem readonly=1
 
 rmmod nova
-insmod nova.ko measure_timing=0 inplace_data_updates=1 replica_metadata=1 metadata_csum=1 unsafe_metadata=0 \
+insmod nova.ko measure_timing=0 inplace_data_updates=0 replica_metadata=1 metadata_csum=1 unsafe_metadata=0 \
 			wprotect=1 mmap_cow=1 data_csum=1 data_parity=1 dram_struct_csum=1
 
 sleep 1
